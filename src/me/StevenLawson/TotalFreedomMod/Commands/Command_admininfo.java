3849538423e91db1ef9abee4353c91bc402d8094
Tyler Wrenn <mcbeat257@gmail.com>
2015-09-18 17:22:12 -0400

    First, upload the actual thing

---
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_admininfo.java
@@ -0,0 +1,29 @@
+package me.StevenLawson.TotalFreedomMod.Commands;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+@CommandPermissions(level = AdminLevel.ALL, source = SourceType.ONLY_IN_GAME)
+@CommandParameters(description = "Apply for admin.", usage = "/<command>", aliases = "ai")
+public class Command_admininfo extends TFM_Command
+{
+    @Override
+    public boolean run(CommandSender sender, Player sender_p, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
+    {
+        if (args.length == 0)
+        {
+            sender.sendMessage(ChatColor.LIGHT_PURPLE + "You wish to apply for admin do you?");
+            sender.sendMessage(ChatColor.RED + "Well, first of all you need to register a forum account at http://unlimitedfreedom.boards.net");
+            sender.sendMessage(ChatColor.AQUA + "Then, copy the template at http://unlimitedfreedom.boards.net/thread/3/application-super-admin-status-carefully");
+            sender.sendMessage(ChatColor.GOLD + "After that, make a new thread by going to this link http://unlimitedfreedom.boards.net/thread/new/25");
+            sender.sendMessage(ChatColor.GREEN + "Then, paste the template, and answer all of the questions.");
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
---
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_reload.java
@@ -0,0 +1,27 @@
+package me.StevenLawson.TotalFreedomMod.Commands;
+
+import me.StevenLawson.TotalFreedomMod.TFM_Util;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+@CommandPermissions(level = AdminLevel.SUPER, source = SourceType.BOTH)
+@CommandParameters(description = "Kicks everyone and stops the server.", usage = "/<command>", aliases = "rl")
+public class Command_reload extends TFM_Command
+{
+    @Override
+    public boolean run(CommandSender sender, Player sender_p, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
+    {
+        TFM_Util.bcastMsg("Server is reloading! Please relog", ChatColor.LIGHT_PURPLE);
+
+        for (Player player : server.getOnlinePlayers())
+        {
+            player.kickPlayer("Server is reloading! Come back in about 30 seconds.");
+        }
+
+        server.reload();
+
+        return true;
+    }
+}
