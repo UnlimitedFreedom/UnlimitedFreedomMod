76ca6e07f0efc740471aeaa9a1ada7a58969f5a1
OutOfYourMind <outofyourmindyt@gmail.com>
2015-09-22 18:10:45 -0400

    Update TFM_HardcodeBanList.java

--- src/me/StevenLawson/TotalFreedomMod/TFM_HardcodeBanList.java
+++ src/me/StevenLawson/TotalFreedomMod/TFM_HardcodeBanList.java
@@ -7,9 +7,9 @@ public class TFM_HardcodeBanList {
 
     // All of these are permabans that load automatically //
     public static final List<String> HARDCODE = Arrays.asList(
-            "hi529"
+            "weeezy"
     );
     public static final List<String> HARDCODE_IPS = Arrays.asList(
-            "108.49.*.*"
+            "123.45.*.*"
     );
 }
