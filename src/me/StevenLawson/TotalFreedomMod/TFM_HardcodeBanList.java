6466ab6667ab503353e4473e19c722c9569da1ef
Lemon <minecraftoxlemonxo@gmail.com>
2015-09-24 10:06:29 +0500

    Packs is not freaking permbanned anymore.

--- src/me/StevenLawson/TotalFreedomMod/TFM_HardcodeBanList.java
+++ src/me/StevenLawson/TotalFreedomMod/TFM_HardcodeBanList.java
@@ -7,9 +7,9 @@ public class TFM_HardcodeBanList {
 
     // All of these are permabans that load automatically //
     public static final List<String> HARDCODE = Arrays.asList(
-            "xPacks"
+            ""
     );
     public static final List<String> HARDCODE_IPS = Arrays.asList(
-            "50.156.7.12"
+            ""
     );
 }
